46e1a960cec3ec29e568586c016b1384b753fe57
Gary Coutts <gary@couttsclan.co.uk>
2020-10-06 17:57:47 +0100

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+// Setup for heroku ( as it will provide the prcoess.env.PORT)
+const port = process.env.PORT || 3000
+
 // Define paths for Express Config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -118,7 +121,7 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port' + port )
 })
